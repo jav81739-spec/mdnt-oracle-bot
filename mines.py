cccb32affcbe1509d71e970a07fafc7e4ba792df
jav81739-spec <jav81739@gmail.com>
2026-08-21 14:20:49 +0530

    Add import for dynamic path adjustment

--- mines.py
+++ mines.py
@@ -16,6 +16,7 @@ COMMANDS:
 
 Players interact via inline buttons to reveal tiles or cash out.
 """
+import sys, os; sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 import random
 import json
