9b7c662d2e5c521266430fd8b58d25a8c0205747
jav81739-spec <jav81739@gmail.com>
2026-08-21 20:56:09 +0530

    Add imports for handlers and chat modules

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'handlers')))
+from chat import *
+from oracle_aesthetic import *
 import http.server
 import socketserver
 import threading
