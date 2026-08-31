765019dfc9cfab34b6cf467f1c8d6e8db42d271c
jav81739-spec <jav81739@gmail.com>
2026-08-31 10:09:22 +0530

    Correct reply-path regression assertion

--- tests/test_final_integration_safety.py
+++ tests/test_final_integration_safety.py
@@ -12,4 +12,4 @@ def test_legacy_storage_exposes_chat_lock():
 
 def test_canonical_router_is_reply_based():
     from midnight_oracle.handlers.message_handler import MessageRouter
-    assert 'reply_to_message_id' in MessageRouter._reply.__code__.co_names
+    assert 'reply_to_message_id' in MessageRouter._reply.__code__.co_consts
