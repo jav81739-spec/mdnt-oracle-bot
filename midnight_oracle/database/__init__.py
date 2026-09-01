b31de62b1ec2587a228522cc923a03e8264022a7
jav81739-spec <jav81739@gmail.com>
2026-09-01 15:15:57 +0530

    build: initialize database package

---
+++ midnight_oracle/database/__init__.py
@@ -0,0 +1,5 @@
+"""Database package for Midnight Oracle."""
+
+from .models import Base, Conversation, GroupMemory, OracleProphecy, RitualLog, User
+
+__all__ = ["Base", "Conversation", "GroupMemory", "OracleProphecy", "RitualLog", "User"]
